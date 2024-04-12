f3b1de68f30d68096389fdf2a06c6d52d256857f
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2024-04-13 00:17:09 +0200

    Create mySQL.php

---
+++ mySQL.php
@@ -0,0 +1,33 @@
+<?php
+class DBCon {
+	private $server = "localhost";
+	private $user = "root";
+	private $password = "";
+	private $database = "test";
+	public $database_handle;
+
+	public function __construct() {
+		$this -> database_handle = new mysqli($this -> server, $this -> user, $this -> password, $this -> database);
+	}
+
+	public function GetTable($nq) {
+		$result = mysqli_query($this -> database_handle, "SELECT * FROM tabla WHERE NAME='$nq';");
+
+		if (!mysqli_num_rows($result)) {
+			echo "Biztos hogy helyesen irtad be?";
+		}
+
+		while ($row = mysqli_fetch_assoc($result)) {
+			foreach ($row as $key => $val) {
+				echo "$key = $val <br>";
+			}
+		}
+
+		mysqli_free_result($result);
+		mysqli_close($this -> database_handle);
+	}
+}
+
+$DBCon = new DBCon();
+$DBCon -> GetTable($_POST['name']);
+?>
