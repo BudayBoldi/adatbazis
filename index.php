9974b95e859bed82ecfc38253325ef1c0e4b17fb
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2024-04-13 00:15:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,10 @@
-<form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+<form method="POST" action="mySQL.php">
 	Neved: <input type="text" name="name">
 	<input type="submit" value="Ok">
 </form>
 
 <?php
-class DBCon {
-	private $server = "localhost";
-	private $user = "root";
-	private $password = "";
-	private $database = "test";
-	public $database_handle;
-
-	public function __construct() {
-		$this -> database_handle = new mysqli($this -> server, $this -> user, $this -> password, $this -> database);
-	}
-
-	public function GetTable($nq) {
-		$result = mysqli_query($this -> database_handle, "SELECT * FROM tabla WHERE NAME='$nq';");
-
-		if (!mysqli_num_rows($result)) {
-			echo "Biztos hogy helyesen irtad be?";
-		}
-
-		while ($row = mysqli_fetch_assoc($result)) {
-			foreach ($row as $key => $val) {
-				echo "$key = $val <br>";
-			}
-		}
-
-		mysqli_free_result($result);
-		mysqli_close($this -> database_handle);
-	}
-}
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = htmlspecialchars($_POST['name']);
-  $DBCon = new DBCon();
-  $DBCon -> GetTable($name);
 }
 ?>
