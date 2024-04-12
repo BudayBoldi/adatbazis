e2933df8fe539ebfaa5ae781db6aacaf5adc031e
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2024-04-13 00:24:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
-<form method="POST" action="mySQL.php">
+<form method="POST" action="#">
 	Neved: <input type="text" name="name">
 	<input type="submit" value="Ok">
 </form>
 
 <?php
+include 'mySQL.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = htmlspecialchars($_POST['name']);
+  $DBCon = new DBCon();
+  $DBCon -> GetTable($_POST['name']);
 }
 ?>
