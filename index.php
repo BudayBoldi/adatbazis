24155ba5eed9c24532cfcba61ff0e1d2f3f93999
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2024-04-15 18:28:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <form method="POST" action="#">
-	Neved: <input type="text" name="name">
-	<input type="submit" value="Ok">
+	Neved: <input type="text" name="name" style="font-size: large; border: 0; border-bottom: 2px solid #333; outline: none">
+	<input type="submit" value="Ok" style="font-size: large; border: 0; border-bottom: 2px solid #333; background: transparent">
 </form>
 
 <?php
@@ -9,6 +9,6 @@ include 'mySQL.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = htmlspecialchars($_POST['name']);
   $DBCon = new DBCon();
-  $DBCon -> GetTable($_POST['name']);
+  $DBCon -> GetTable($name);
 }
 ?>
