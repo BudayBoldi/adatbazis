7e515ccc5dbf6a1b2ba2933b001d95ac8d9553d7
BudayBoldi <78080884+BudayBoldi@users.noreply.github.com>
2025-01-30 21:21:27 +0100

    Update mySQL.php

--- mySQL.php
+++ mySQL.php
@@ -14,26 +14,36 @@ class DBCon {
 		$result = mysqli_query($this -> database_handle, "SELECT * FROM tabla WHERE NAME='$nq';");
 
 		if (!mysqli_num_rows($result)) {
-			echo "Biztos hogy helyesen irtad be?";
+			echo "<res>Biztos hogy helyesen irtad be?</res>";
 		}
 
 		while ($row = mysqli_fetch_assoc($result)) {
 			foreach ($row as $key => $val) {
-				echo "$key = $val <br>";
+				echo "<res>$key = $val</res> <br>";
 			}
 		}
 
 		mysqli_free_result($result);
 		mysqli_close($this -> database_handle);
 	}
-	public function WriteTable($nw, $adw, $aw) {
-		if(mysqli_query($this -> database_handle, "INSERT INTO tabla VALUES (null, '$nw', '$adw', '$aw');")) {
-			echo "Success!";
-		} else {
-			echo "Error: " . mysqli_error($this -> database_handle);
-		}
+	public function WriteTable($nw, $adw, $aw, $del) {
+		if(!$del) {
+			if(mysqli_query($this -> database_handle, "INSERT INTO tabla VALUES (null, '$nw', '$adw', '$aw');")) {
+				echo "Belerakva!";
+			} else {
+				echo "Hiba: " . mysqli_error($this -> database_handle);
+			}
 
-		mysqli_close($this -> database_handle);
+			mysqli_close($this -> database_handle);
+		} else {
+			if(mysqli_query($this -> database_handle, "DELETE FROM tabla WHERE NAME='$nw';")) {
+				echo "Kiszedve!";
+			} else {
+				echo "Hiba: " . mysqli_error($this -> database_handle);
+			}
+
+			mysqli_close($this -> database_handle);
+		}
 	}
 }
 ?>
